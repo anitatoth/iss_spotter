d67c8d92f89cdc344093957ee2922cf6901fb78b
Anita Toth <haluska99@hotmail.com>
2021-02-12 02:20:33 +0000

    created waterfall

--- iss.js
+++ iss.js
@@ -1,5 +1,39 @@
 const request = require('request');
 
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+
+
+module.exports = { nextISSTimesForMyLocation };
 
 /**
  * Makes a single API request to retrieve the user's IP address.
@@ -20,7 +54,6 @@ const fetchMyIP = function(callback) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
-
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
@@ -87,5 +120,5 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
 
